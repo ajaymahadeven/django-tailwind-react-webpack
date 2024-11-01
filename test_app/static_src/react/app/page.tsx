d97822d26d6a3f23f9b0daf76f4b8640fc0916e6
thenameisajay <amahadeven@economicdatasciences.com>
2024-11-01 15:11:01 +0000

    navigate between pages

--- test_app/static_src/react/app/page.tsx
+++ test_app/static_src/react/app/page.tsx
@@ -9,13 +9,21 @@ const root: Root = createRoot(container);
 
 root.render(
   <div className="flex min-h-screen w-full flex-col">
-    <div className="flex-1 flex items-center justify-center">
+    <div className="flex-1 flex-col flex items-center justify-center">
       <h1 className="text-4xl font-bold">Hello, React!</h1>
-      <a href="/admin">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Admin
-        </button>
-      </a>
+      <section className="mt-4 flex-row space-x-2">
+        <a href="/">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Home
+          </button>
+        </a>
+
+        <a href="/admin">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Admin
+          </button>
+        </a>
+      </section>
     </div>
   </div>
 );
