2980e1d1ba42183c81511a1c1a34454ab13e1e07
thenameisajay <m.ajaypradeep@icloud.com>
2024-11-02 00:07:02 +0000

    new changes

--- test_app/static_src/react/app/page.tsx
+++ test_app/static_src/react/app/page.tsx
@@ -10,7 +10,7 @@ const root: Root = createRoot(container);
 root.render(
   <div className="flex min-h-screen w-full flex-col">
     <div className="flex-1 flex-col flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Django + Tailwind + React =  🧡 </h1>
+      <h1 className="text-5xl">Django + Tailwind + React = 🧡 </h1>
       <section className="mt-4 flex-row space-x-2">
         <a href="/">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
