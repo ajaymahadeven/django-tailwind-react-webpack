0fb21bb980c64e4f6179d9d2b14e8fad1a6e5213
Ajay Mahadeven <m.ajaypradeep@outlook.com>
2025-03-17 20:37:55 +0000

    update production changes

--- test_app/static_src/webpack.production.config.ts
+++ test_app/static_src/webpack.production.config.ts
@@ -1,10 +1,9 @@
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import path from 'path';
-import TerserPlugin from 'terser-webpack-plugin';
-import webpack, { type Configuration } from 'webpack';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import TerserPlugin from "terser-webpack-plugin";
+import webpack, { type Configuration } from "webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 const buildDirectory = "../static/compiled/";
 
@@ -38,16 +37,37 @@ const config: ExtendedConfiguration = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
         },
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "style-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 8kb
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
@@ -56,10 +76,15 @@ const config: ExtendedConfiguration = {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          ecma: 5,
+          ecma: 2020,
           compress: {
             unused: true,
             dead_code: true,
+            drop_console: true,
+            drop_debugger: true,
+          },
+          format: {
+            comments: false,
           },
         },
         parallel: true,
@@ -68,7 +93,10 @@ const config: ExtendedConfiguration = {
     ],
   },
   plugins: [
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+    }),
     new webpack.ProvidePlugin({
       React: "react",
     }),
@@ -80,7 +108,15 @@ const config: ExtendedConfiguration = {
       verbose: true,
       dry: false,
     }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
   ],
+  performance: {
+    hints: "warning",
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
 
 export default config;
